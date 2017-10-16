6637301737b81af6e910060eb3c57e2b69930fff
Adam Clayman <adam@clayman.org>
2017-10-16 01:00:45 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction (name)
 end
 
 def introduction_with_language (name, language) {
-  puts `This is a phrase with a #{name} and a #{language}.`
+  puts `Hi, my name is #{name} and I am learning to program in #{language}.`
 }
