e8530edf70de2a2111e7d2758b8442ed16ec8eec
Adam Clayman <adam@clayman.org>
2017-10-16 01:00:14 +0000

    automatically backed up by learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction (name)
+  puts `This is a phrase with #{name}.`
+end
+
+def introduction_with_language (name, language) {
+  puts `This is a phrase with a #{name} and a #{language}.`
+}
